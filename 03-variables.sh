9c97c388bee1a616bbf220b87d14637351811440
Gowsiya <50201437+Gowsiya@users.noreply.github.com>
2025-02-04 17:12:17 +0530

    converstaion added

--- 03-variables.sh
+++ 03-variables.sh
@@ -3,7 +3,7 @@
 PERSON1=Rahim
 PERSON2=Robert
 
-echo "PERSON1: Hi $PERSON2"
-echo "PERSON2: Hello PERSON1"
-echo "PERSON1: How are you doing?"
-echo "PERSON2: Am fine thank you"
+echo "$PERSON1: Hi $PERSON2"
+echo "$PERSON2: Hello $PERSON1"
+echo "$PERSON1: How are you doing?"
+echo "$PERSON2: Am fine thank you"
