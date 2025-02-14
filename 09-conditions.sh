b3779d5e0cdbc211fa49519eaa294bcd3169e80d
Gowsiya <50201437+Gowsiya@users.noreply.github.com>
2025-02-14 20:57:43 +0530

    shell script

---
+++ 09-conditions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Enter the Number:"
+read -s "NUMBER"
+
+if [$NUMBER -gt 100]
+then
+    echo "$NUMBER is greater than 100"
+else
+    echo "$NUMBER is less than or equal to 100"
+fi
