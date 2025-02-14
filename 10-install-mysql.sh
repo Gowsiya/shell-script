81ebb88ac648a447c4cb3a487da081718b1b6e81
Gowsiya <50201437+Gowsiya@users.noreply.github.com>
2025-02-14 21:32:31 +0530

    shell script

--- 10-install-mysql.sh
+++ 10-install-mysql.sh
@@ -4,7 +4,7 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-    echo "ERROR: user must have the root access to execute the command"
+    echo "ERROR: user must have the root access to execute this command"
     exit 1
 fi
 dnf install mysql -y
