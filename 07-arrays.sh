7aa76a79271096718da76accf921009610c4eb96
Gowsiya <50201437+Gowsiya@users.noreply.github.com>
2025-02-14 13:37:33 +0530

    shell script

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -3,3 +3,6 @@
 MOVIES=("devara" "rrr" "orange")
 
 echo "First Movie is : ${MOVIES[0]}"
+echo "Second Movie is : ${MOVIES[1]}"
+echo "Third Movie is : ${MOVIES[2]}"
+echo "ALL Movies are : ${MOVIES[@]}"
