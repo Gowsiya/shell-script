9f5fee8207c0e139b1df9a0e2e7d00f174d46f44
Gowsiya <50201437+Gowsiya@users.noreply.github.com>
2025-02-16 13:08:56 +0530

    shell script

---
+++ 11-functions.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+USERID=(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR: User must have root access to execute this command"
+    exit 1
+fi
+
+VALIDATE () {
+if [ $1 -ne 0 ]
+    then
+        echo " $2 ... Failure"
+        exit 1
+    else 
+        echo "$2 ... Success"
+    fi
+}
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then 
+    dnf install mysql -y
+    VALIDATE $? "Installing Mysql"
+else
+    echo "Mysql is already ... Installed"
+
+
