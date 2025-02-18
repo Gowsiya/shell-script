58ad0044dc03d313d3087be6c0e74522bad89287
Gowsiya <50201437+Gowsiya@users.noreply.github.com>
2025-02-18 22:39:20 +0530

    shell-script

---
+++ 17-Files-to-delete.sh
@@ -0,0 +1,23 @@
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+SOURCE_PATH=/home/ec2-user/app-logs
+mkdir -p /var/log/shellscript-logs
+
+LOGS_FOLDER=/var/log/shellscript-logs
+LOG_FILE=$(echo $0 | cut -d "." f)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME=LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log
+
+echo "Script started executing at: $TIMESTAMP"
+
+FILES_TO_BE_DELETED=$(find $SOURCE_PATH -name "*.log" -mtime +30)
+echo "Files to be deleted: $FILES_TO_BE_DELETED"
+
+while read -r filepath
+do
+    echo $filepath
+done <<<$FILES_TO_BE_DELETED
+
