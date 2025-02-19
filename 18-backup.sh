7b0d0e2ee52363132006be5cdc15d19df5ebad27
Gowsiya <50201437+Gowsiya@users.noreply.github.com>
2025-02-19 12:34:41 +0530

    shell-script

--- 18-backup.sh
+++ 18-backup.sh
@@ -15,7 +15,8 @@ TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME=$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log
 
 USAGE () {
-    echo " USAGE :: backup <SOURCE_DIR> <DES_DIR> <Days(optional)>
+    echo " USAGE :: backup <SOURCE_DIR> <DES_DIR> <Days(optional)>"
+    exit 1
 }
 
 echo "Script started executing at: $TIMESTAMP"
